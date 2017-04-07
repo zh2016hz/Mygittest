fa3d08413719610164cdc91cc4ec8f766c245afa
zhou <ZHOUHAO046@pingan.com.cn>
2017-04-07 17:12:20 +0800

    enter the commit message for your changes. Lines starting

--- app/src/main/java/com/example/aa/gittest/MainActivity.java
+++ app/src/main/java/com/example/aa/gittest/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//dsadad
        android.util.Log.e("第一次修改!!!!!!!", "onCreate: " );
     }
 }
