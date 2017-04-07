fe601778bc4f8db60f75b4393de9604608318deb
zhou <ZHOUHAO046@pingan.com.cn>
2017-04-07 10:57:10 +0800

    修改忽略文件

---
+++ app/src/main/java/com/example/aa/gittest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.aa.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        android.util.Log.e("第一次修改!!!!!!!", "onCreate: " );
+    }
+}
