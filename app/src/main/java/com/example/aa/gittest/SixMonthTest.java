ca05f4140845690bc29fbb752e769286fd492f7e
zhouhao9@xiaoniu66.com <zhouhao9@xiaoniu66.com>
2018-06-01 15:33:18 +0800

    尼玛 6月 的  测试

---
+++ app/src/main/java/com/example/aa/gittest/SixMonthTest.java
@@ -0,0 +1,19 @@
+package com.example.aa.gittest;
+
+/**
+ * 文件描述：
+ * Created by  xn069392
+ * 创建时间    2018/6/1
+ */
+
+public class SixMonthTest {
+    StringBuffer sb = new StringBuffer();
+
+    public String getString() {
+        for (int i = 0; i < 100; i++) {
+            sb.append(" " + i);
+        }
+        return sb.toString();
+    }
+
+}
